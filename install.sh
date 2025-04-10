59f887c7e7973af390a28b2df4871b9e783236f3
Liam Colle <liam.colle@epitech.eu>
2025-04-10 19:59:12 +0200

    feat: Add official installer

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+GREEN="\e[32m"
+YELLOW="\e[33m"
+RED="\e[31m"
+ENDCOLOR="\e[0m"
+
+RM=rm
+GIT_COMMAND=git
+PYTHON3_COMMAND=python3
+
+## Check required commands
+if ! command -v $GIT_COMMAND 2>&1 >/dev/null
+then
+    echo -e "${YELLOW}${GIT_COMMAND}${RED} could not be found!${ENDCOLOR}\nPlease install the ${YELLOW}${GIT_COMMAND}${ENDCOLOR} package."
+    exit 1
+fi
+
+if ! command -v $PYTHON3_COMMAND 2>&1 >/dev/null
+then
+    echo -e "${YELLOW}${PYTHON3_COMMAND}${RED} could not be found!${ENDCOLOR}\nPlease install the ${YELLOW}${PYTHON3_COMMAND}${ENDCOLOR} package."
+    exit 1
+fi
+
+$RM -rf /tmp/grindme-installer
+if ! git clone --depth=1 https://github.com/liam-colle-archivist/GrindMe.git /tmp/grindme-installer 2> /dev/null; then
+    echo -e "${RED}${GIT_COMMAND} could not clone the repository.\n${YELLOW}Check your connection to github.${ENDCOLOR}"
+    exit 1
+fi
+/tmp/grindme-installer/install_system.sh
+$RM -rf /tmp/grindme-installer
